af8a509a1f99bf659a705d6769f4b0e02567d38d
tomas hyhlik <tomaskater@senzma.cz>
2019-10-30 01:37:36 +0100

    edited json

--- json/json_read_tempPress/main.c
+++ json/json_read_tempPress/main.c
@@ -6,59 +6,51 @@
 #include <stdint.h>
 
 
-bool json_getVal(char* buffer_out, const char* buffer_in, const char* name);
+bool json_getFloat(float* f, const char* searchedVarName,const char* buffer, size_t buffer_len);
 const char jsonSplitFactors[] = ",{}";
 
-const char ident_temp[] = "temperature";
-const char ident_press[] = "pressure";
+const char* ident_temp = "temperature";
+const char* ident_press = "pressure";
 
 
 ////////////////////////////////////////////////////////////////////
-int main ()
+int main (void)
 {
     /* sample data */
-    char data[] = "{\"temperature\": 27.74,\"pressure\": 1000.51}";
+    char processingData[] = "{\"temperature\": 27.74,\"pressure\": 1000.51}";
+    printf("Processing data: %s\n", processingData);
 
+    float temp, press;
 
-    printf("data: %s\n", data);
-
-    if (json_getVal(temp_str, data, ident_temp)) {
-        float temperature = strtof(temp_str, NULL);
-        printf("Float temp: %.2f\n", temperature);
+    if (json_getFloat(&temp, ident_temp, processingData, strlen(processingData))) {
+        printf("Temp: %.2f\n", temp);
+    } else {
+        printf("ERROR:1\n");
     }
-
-    if (json_getVal(press_str, data, ident_press)) {
-        float pressure = strtof(press_str, NULL);
-        printf("Float temp: %.2f\n", pressure);
+    if (json_getFloat(&press, ident_press, processingData, strlen(processingData))) {
+        printf("Press: %.2f\n", press);
+    } else {
+        printf("ERROR:2\n");
     }
-
+    
     return 0;
 }
+
 ////////////////////////////////////////////////////////////////////
-/*
- * @brief search in the @param buffer_in for the @param name and the
- *  found value string put in the buffer_out and return true if found
- * @note all the strings must have the ending character '\0'
- */
-bool json_getVal(char* buffer_out, const char* buffer_in, const char* name)
+bool json_getFloat(float* f, const char* searchedVarName,const char* buffer, size_t buffer_len)
 {
     char *msgStart, *pch;
+    char data[20];
+    char buff[buffer_len];
+    memcpy(buff, buffer, buffer_len);
 
-    char* data_in = malloc(strlen(buffer_in)+1);
-    if (data_in != NULL) {
-
-        memcpy(data_in, buffer_in, strlen(buffer_in));
-        msgStart = strstr (data_in, name);
-        if (msgStart != NULL) {
-            msgStart += strlen(name) + 2;
-            pch = strtok(msgStart, jsonSplitFactors);
-
-            strcpy(buffer_out, pch);
-            free(data_in);
-            return true;
-        } 
-    }
-    free(data_in);
+    msgStart = strstr (buff, searchedVarName);
+    if (msgStart != NULL) {
+        msgStart += strlen(searchedVarName) + 2;
+        pch = strtok(msgStart, jsonSplitFactors);
+        *f = strtof(pch, NULL);
+        return true;
+    } 
     return false;
 }
-
+////////////////////////////////////////////////////////////////////
