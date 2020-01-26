92ece1053147d0b6d7454756ec6890e3700c897d
Hyhlík, Tomáš, Bc <hyhlito1@fel.cvut.cz>
2020-01-26 22:23:03 +0100

    nvm

--- printf/01.c
+++ printf/01.c
@@ -5,13 +5,11 @@ int main(void)
 {
 	printf("AppStart...................\n");
 
-	//////////////////////////////////////////
 	printf("________char______\n");
 	char a = 'a';
 	printf("char: %c, has dec ASCII value: %d, "
 			"and hex ASCII value: %.2X\n", a, a, a);
 
-	//////////////////////////////////////////
 	printf("_________float_&_double_____\n");
 	double f = 12.3456789;
 	printf("The whole float is: %f\n", f);
